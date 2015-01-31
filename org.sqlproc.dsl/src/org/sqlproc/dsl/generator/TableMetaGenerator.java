39e91dc71200e6de804e66bcf096ac33346a7d25
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2015-01-31 13:53:02 +0100

    Issue #81: Grammar 2.0

--- org.sqlproc.dsl/src/org/sqlproc/dsl/generator/TableMetaGenerator.java
+++ org.sqlproc.dsl/src/org/sqlproc/dsl/generator/TableMetaGenerator.java
@@ -1001,7 +1001,8 @@ public class TableMetaGenerator extends TablePojoGenerator {
         }
         if (pojoName == null)
             pojoName = pojo;
-        buffer.append(",inx=").append((dispName != null) ? dispName : tableToCamelCase(pojoName));
+        buffer.append(",").append(Constants.IDENTIFIER_USAGE_EXTENDED).append("=")
+                .append((dispName != null) ? dispName : tableToCamelCase(pojoName));
         buffer.append(")=");
         buffer.append("\n  ");
         PojoAttribute resultSetAttribute = resultSetAttribute(pojoName, isFunction);
@@ -1074,7 +1075,8 @@ public class TableMetaGenerator extends TablePojoGenerator {
                 String outPojoName = tableNames.get(outPojo);
                 if (outPojoName == null)
                     outPojoName = outPojo;
-                buffer.append(",outx=").append(tableToCamelCase(outPojoName));
+                buffer.append(",").append(Constants.COLUMN_USAGE_EXTENDED).append("=")
+                        .append(tableToCamelCase(outPojoName));
                 buffer.append(")=\n ");
                 for (Map.Entry<String, PojoAttribute> pentry : pojos.get(outPojo).entrySet()) {
                     // System.out.println("  RRR " + pentry.getKey());
@@ -1106,7 +1108,8 @@ public class TableMetaGenerator extends TablePojoGenerator {
                 String outPojoName = tableNames.get(outPojo);
                 if (outPojoName == null)
                     outPojoName = outPojo;
-                buffer.append(",outx=").append(tableToCamelCase(outPojoName));
+                buffer.append(",").append(Constants.COLUMN_USAGE_EXTENDED).append("=")
+                        .append(tableToCamelCase(outPojoName));
                 buffer.append(")=\n ");
                 for (Map.Entry<String, PojoAttribute> pentry : pojos.get(outPojo).entrySet()) {
                     // System.out.println("  RRR " + pentry.getKey());
@@ -1165,7 +1168,7 @@ public class TableMetaGenerator extends TablePojoGenerator {
         String pojoName = tableNames.get(pojo);
         if (pojoName == null)
             pojoName = pojo;
-        buffer.append(",inx=").append(tableToCamelCase(pojoName));
+        buffer.append(",").append(Constants.IDENTIFIER_USAGE_EXTENDED).append("=").append(tableToCamelCase(pojoName));
         buffer.append(")=");
         buffer.append("\n  ");
         buffer.append("select ").append(pojo).append("(");
@@ -1193,7 +1196,7 @@ public class TableMetaGenerator extends TablePojoGenerator {
         buffer.append("\n").append("FUN_").append(pojo.toUpperCase()).append("(OUT");
         if (metaMakeItFinal)
             buffer.append(",final=");
-        buffer.append(",outx=").append(tableToCamelCase(pojoName));
+        buffer.append(",").append(Constants.COLUMN_USAGE_EXTENDED).append("=").append(tableToCamelCase(pojoName));
         buffer.append(")=\n ");
         buffer.append("  1$result\n;");
         buffer.append("\n");
