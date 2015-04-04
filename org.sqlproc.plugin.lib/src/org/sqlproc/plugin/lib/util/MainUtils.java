da4851e2f3f29826870bfc6f33722b7b1c9bdaca
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2015-04-04 08:31:26 +0200

    Issue #88: SQLMEP, SQLMOP unification

--- org.sqlproc.plugin.lib/src/org/sqlproc/plugin/lib/util/MainUtils.java
+++ org.sqlproc.plugin.lib/src/org/sqlproc/plugin/lib/util/MainUtils.java
@@ -19,81 +19,83 @@ import org.eclipse.xtext.validation.Issue;
 
 public class MainUtils {
 
-	public static final int OK = 0;
-	public static final int ERROR = 1;
-	public static final int REFERENCE_ERROR = 2;
-	public static final String REFERENCE_MSG = "Couldn't resolve reference to JvmType";
+    public static final int OK = 0;
+    public static final int ERROR = 1;
+    public static final int REFERENCE_ERROR = 2;
+    public static final String REFERENCE_MSG = "Couldn't resolve reference to JvmType";
 
-	public static int isValid(Resource resource, Set<String> failedReferences, IResourceValidator validator)
-	        throws IOException {
-		int result = OK;
-		resource.load(null);
-		List<Issue> list = validator.validate(resource, CheckMode.ALL, CancelIndicator.NullImpl);
-		if (!list.isEmpty()) {
-			for (Issue issue : list) {
-				String msg = issue.toString();
-				System.err.println(msg);
-				if (issue.getSeverity() == Severity.ERROR) {
-					if (msg.indexOf(REFERENCE_MSG) < 0)
-						result = ERROR;
-					else {
-						if (result == OK)
-							result = REFERENCE_ERROR;
-						if (failedReferences != null) {
-							msg = msg.substring(REFERENCE_MSG.length(), msg.length());
-							int ix = msg.indexOf("'");
-							if (ix >= 0)
-								msg = msg.substring(ix + 1, msg.length());
-							ix = msg.indexOf("'");
-							if (ix >= 0)
-								msg = msg.substring(0, ix);
-							failedReferences.add(msg);
-						}
-					}
-				}
-			}
-		}
-		return result;
-	}
+    public static int isValid(Resource resource, Set<String> failedReferences, IResourceValidator validator)
+            throws IOException {
+        int result = OK;
+        resource.load(null);
+        List<Issue> list = validator.validate(resource, CheckMode.ALL, CancelIndicator.NullImpl);
+        if (!list.isEmpty()) {
+            for (Issue issue : list) {
+                String msg = issue.toString();
+                System.err.println(msg);
+                if (issue.getSeverity() == Severity.ERROR) {
+                    if (msg.indexOf(REFERENCE_MSG) < 0)
+                        result = ERROR;
+                    else {
+                        if (result == OK)
+                            result = REFERENCE_ERROR;
+                        if (failedReferences != null) {
+                            msg = msg.substring(REFERENCE_MSG.length(), msg.length());
+                            int ix = msg.indexOf("'");
+                            if (ix >= 0)
+                                msg = msg.substring(ix + 1, msg.length());
+                            ix = msg.indexOf("'");
+                            if (ix >= 0)
+                                msg = msg.substring(0, ix);
+                            failedReferences.add(msg);
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
 
-	public static String loadResourceToString(Resource resource) throws IOException {
-		resource.load(null);
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		resource.save(baos, null);
-		return baos.toString();
-	}
+    public static String loadResourceToString(Resource resource) throws IOException {
+        resource.load(null);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        resource.save(baos, null);
+        return baos.toString();
+    }
 
-	public static String handleResourceReferences(Resource resource, Set<String> failedReferences) throws IOException {
-		String content = loadResourceToString(resource);
-		for (String ref : failedReferences) {
-			content = content.replaceAll(ref, "::" + ref);
-		}
-		content = content.replaceAll("::::", "::");
-		return content;
-	}
+    public static String handleResourceReferences(Resource resource, Set<String> failedReferences) throws IOException {
+        String content = loadResourceToString(resource);
+        for (String ref : failedReferences) {
+            content = content.replaceAll(ref, "::" + ref);
+        }
+        content = content.replaceAll("::::", "::");
+        content = content.replaceAll("::::", "::");
+        content = content.replaceAll("::::", "::");
+        return content;
+    }
 
-	public static Resource reloadResourceFromString(String content, ResourceSet resourceSet,
-	        IResourceFactory resourceFactory, String suffix) throws IOException {
-		InputStream is = new LazyStringInputStream(content);
-		Resource resource = resourceFactory.createResource(computeUnusedUri(resourceSet, suffix));
-		resourceSet.getResources().add(resource);
-		resource.load(is, null);
-		return resource;
-	}
+    public static Resource reloadResourceFromString(String content, ResourceSet resourceSet,
+            IResourceFactory resourceFactory, String suffix) throws IOException {
+        InputStream is = new LazyStringInputStream(content);
+        Resource resource = resourceFactory.createResource(computeUnusedUri(resourceSet, suffix));
+        resourceSet.getResources().add(resource);
+        resource.load(is, null);
+        return resource;
+    }
 
-	public static URI computeUnusedUri(ResourceSet resourceSet, String suffix) {
-		String name = "__synthetic";
-		for (int i = 0; i < Integer.MAX_VALUE; i++) {
-			URI syntheticUri = URI.createURI(name + i + "." + suffix);
-			if (resourceSet.getResource(syntheticUri, false) == null)
-				return syntheticUri;
-		}
-		throw new IllegalStateException();
-	}
+    public static URI computeUnusedUri(ResourceSet resourceSet, String suffix) {
+        String name = "__synthetic";
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            URI syntheticUri = URI.createURI(name + i + "." + suffix);
+            if (resourceSet.getResource(syntheticUri, false) == null)
+                return syntheticUri;
+        }
+        throw new IllegalStateException();
+    }
 
-	public static String getFile(String source, String file) {
-		if (file.startsWith("/"))
-			return file;
-		return source + file;
-	}
+    public static String getFile(String source, String file) {
+        if (file.startsWith("/"))
+            return file;
+        return source + file;
+    }
 }
