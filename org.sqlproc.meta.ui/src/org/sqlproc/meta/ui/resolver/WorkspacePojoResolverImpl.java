49eff4310d77a0bab0c0a398b62d9ead7fa7120c
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2015-08-25 09:04:42 +0200

    Issue #117: Generate index based on name

--- org.sqlproc.meta.ui/src/org/sqlproc/meta/ui/resolver/WorkspacePojoResolverImpl.java
+++ org.sqlproc.meta.ui/src/org/sqlproc/meta/ui/resolver/WorkspacePojoResolverImpl.java
@@ -1,14 +1,24 @@
 package org.sqlproc.meta.ui.resolver;
 
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import java.io.InputStream;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IContainer;
@@ -21,6 +31,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragment;
@@ -31,23 +42,21 @@ import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.PlatformUI;
 import org.sqlproc.plugin.lib.property.ModelProperty;
-import org.sqlproc.plugin.lib.resolver.BasePojoResolverImpl;
 import org.sqlproc.plugin.lib.resolver.PojoResolver;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 @Singleton
-public class WorkspacePojoResolverImpl extends BasePojoResolverImpl implements PojoResolver {
+public class WorkspacePojoResolverImpl implements PojoResolver {
 
     protected Logger LOGGER = Logger.getLogger(WorkspacePojoResolverImpl.class);
 
     @Inject
     ModelProperty modelProperty;
 
-    protected ModelProperty getModelProperty() {
-        return modelProperty;
-    };
+    private Map<String, URLClassLoader> allLoaders;
+    private Set<String> nokLoaders = new HashSet<String>();
 
     protected void init() {
         LOGGER.info("POJO START");
@@ -84,6 +93,7 @@ public class WorkspacePojoResolverImpl extends BasePojoResolverImpl implements P
             else
                 allLoaders = loaders;
         }
+        allLoaders = loaders;
     }
 
     @SuppressWarnings("unused")
@@ -105,6 +115,145 @@ public class WorkspacePojoResolverImpl extends BasePojoResolverImpl implements P
         URL[] urls = (URL[]) urlList.toArray(new URL[urlList.size()]);
         URLClassLoader classLoader = new URLClassLoader(urls, parentClassLoader);
         return classLoader;
+    }
+
+    @Override
+    public Class<?> loadClass(String name, URI uri) {
+        // platform:/resource/simple-jdbc-dao/src/main/resources/statements.meta
+        String pname = getProjectName(uri);
+        if (allLoaders == null)
+            init();
+        boolean retry = false;
+        if (pname != null) {
+            URLClassLoader loader = allLoaders.get(pname);
+            if (loader != null) {
+                try {
+                    return loader.loadClass(name);
+                } catch (ClassNotFoundException ignore) {
+                }
+                // for the case a new project is opened
+                init();
+                loader = allLoaders.get(pname);
+                if (loader != null) {
+                    try {
+                        return loader.loadClass(name);
+                    } catch (ClassNotFoundException ignore) {
+                        LOGGER.warn("Can't find class '" + name + "' in loader " + loader + " " + uri);
+                    }
+                } else {
+                    retry = true;
+                }
+            } else {
+                retry = true;
+            }
+        }
+        if (!retry)
+            return null;
+        for (Entry<String, URLClassLoader> e : allLoaders.entrySet()) {
+            try {
+                Class<?> clazz = e.getValue().loadClass(name);
+                LOGGER.warn("Found " + name + "(" + uri + ") in " + e.getKey());
+                return clazz;
+            } catch (ClassNotFoundException ignore) {
+            }
+        }
+        // for the case a new project is opened
+        init();
+        for (Entry<String, URLClassLoader> e : allLoaders.entrySet()) {
+            try {
+                Class<?> clazz = e.getValue().loadClass(name);
+                LOGGER.warn("Found " + name + "(" + uri + ") in " + e.getKey());
+                return clazz;
+            } catch (ClassNotFoundException ignore) {
+            }
+        }
+        LOGGER.warn("Can't find class '" + name + "' in any loader " + allLoaders);
+        return null;
+    }
+
+    @Override
+    public PropertyDescriptor[] getPropertyDescriptors(String name, URI uri) {
+        // platform:/resource/simple-jdbc-dao/src/main/resources/statements.meta
+        if (allLoaders == null)
+            init();
+        Class<?> beanClass = loadClass(name, uri);
+        if (beanClass == null)
+            return null;
+
+        // Look up any cached descriptors for this bean class
+        PropertyDescriptor[] descriptors = null;
+        // descriptors = (PropertyDescriptor[]) descriptorsCache.get(beanClass);
+        // if (descriptors != null) {
+        // return (descriptors);
+        // }
+
+        // Introspect the bean and cache the generated descriptors
+        BeanInfo beanInfo = null;
+        try {
+            beanInfo = Introspector.getBeanInfo(beanClass);
+        } catch (IntrospectionException e) {
+            return (new PropertyDescriptor[0]);
+        }
+        descriptors = beanInfo.getPropertyDescriptors();
+        if (descriptors == null) {
+            descriptors = new PropertyDescriptor[0];
+        }
+
+        // descriptorsCache.put(beanClass, descriptors);
+        return descriptors;
+    }
+
+    @Override
+    public Map<String, String> getOrders(String name, URI uri) {
+        // platform:/resource/simple-jdbc-dao/src/main/resources/statements.meta
+        if (allLoaders == null)
+            init();
+        Class<?> beanClass = loadClass(name, uri);
+        if (beanClass == null)
+            return null;
+
+        Map<String, String> orders = new HashMap<>();
+        while (beanClass != null) {
+            Field[] fields = beanClass.getFields();
+            for (Field f : fields) {
+                if (Modifier.isStatic(f.getModifiers())) {
+                    try {
+                        if (f.getType() == int.class)
+                            orders.put(f.getName(), "" + f.getInt(null));
+                        else if (f.getType() == String.class)
+                            orders.put(f.getName(), (String) f.get(null));
+                    } catch (IllegalArgumentException | IllegalAccessException e) {
+                    }
+                }
+            }
+            beanClass = beanClass.getSuperclass();
+        }
+
+        beanClass = loadClass(name, uri);
+        Class<?> orderBeanClass = loadClass(name + "$Order", uri);
+        while (orderBeanClass != null && orderBeanClass.isEnum()) {
+            for (Object en : orderBeanClass.getEnumConstants()) {
+                String ename = en.toString();
+                orders.put(ename, ename);
+            }
+
+            beanClass = beanClass.getSuperclass();
+            if (beanClass == null)
+                break;
+            orderBeanClass = loadClass(beanClass.getName() + "$Order", uri);
+        }
+        return orders;
+    }
+
+    @Override
+    public boolean isResolvePojo(EObject model) {
+        if (!modelProperty.isDoResolvePojo(model)) {
+            return false;
+        }
+        if (allLoaders == null) {
+            init();
+        }
+        return allLoaders != null;
     }
 
     @Override
@@ -180,5 +329,21 @@ public class WorkspacePojoResolverImpl extends BasePojoResolverImpl implements P
         }
         LOGGER.warn("Can't find file '" + filename + "' in project");
         return null;
+    }
+
+    private static final String PREFIX1 = "platform:/resource/";
+    private static final int PLEN1 = PREFIX1.length();
+
+    private String getProjectName(URI uri) {
+        // platform:/resource/simple-jdbc-dao/src/main/resources/statements.meta
+        if (uri == null)
+            return null;
+        String name = uri.toString();
+        if (name.startsWith(PREFIX1))
+            name = name.substring(PLEN1);
+        int ix = name.indexOf("/");
+        if (ix >= 0)
+            name = name.substring(0, ix);
+        return name;
     }
 }
